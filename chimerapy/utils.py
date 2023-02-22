cc7950f87ecf85af10e7fdb8fb48a6e38f0e0cdf
edavalosanaya <davalosaeduardo@gmail.com>
2023-02-21 21:40:43 -0600

    fix(IP without network): Provide default 127.0.0.1 when requesting IP without being connected to a network.

--- chimerapy/utils.py
+++ chimerapy/utils.py
@@ -149,10 +149,14 @@ def get_ip_address() -> str:
 
     # Get gateway of the network
     gws = ni.gateways()
-    default_gw_name = gws["default"][ni.AF_INET][1]
+    try:
+        default_gw_name = gws["default"][ni.AF_INET][1]
+        # Get the ip in the default gateway
+        ip = ni.ifaddresses(default_gw_name)[ni.AF_INET][0]["addr"]
+    except KeyError:
+        logger.warning("ChimeraPy: Couldn't find connected network, using 127.0.0.1")
+        ip = "127.0.0.1"
 
-    # Get the ip in the default gateway
-    ip = ni.ifaddresses(default_gw_name)[ni.AF_INET][0]["addr"]
     return ip
 
 
