6df0c7d37b14f89f0ecc614e3e48e9313568dde7
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2023-03-29 14:07:36 -0500

    Address dir creation repo root when testing. Closes #149 (#150)

--- test/test_manager.py
+++ test/test_manager.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 
 from chimerapy.manager import Manager
+from .conftest import TEST_DATA_DIR
 
 
 def test_manager_logdir_string():
-    manager = Manager(logdir=".", port=0)
-    assert manager.logdir.parent == Path(".").resolve()
+    manager = Manager(logdir=str(TEST_DATA_DIR), port=0)
+    assert manager.logdir.parent == TEST_DATA_DIR
     manager.shutdown()
 
 
 def test_manager_logdir_path():
-    manager = Manager(logdir=Path("."), port=0)
-    assert manager.logdir.parent == Path(".").resolve()
+    manager = Manager(logdir=TEST_DATA_DIR, port=0)
+    assert manager.logdir.parent == TEST_DATA_DIR
     manager.shutdown()
