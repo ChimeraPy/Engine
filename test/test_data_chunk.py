607cf075cd948fa9785c269467245d46a6ccfe1a
edavalosanaya <davalosaeduardo@gmail.com>
2023-08-22 19:11:59 -0500

    feat(test): Added unittest for DataChunk.

--- test/test_data_chunk.py
+++ test/test_data_chunk.py
@@ -18,7 +18,23 @@ def video_data_chunk():
     return data_chunk
 
 
-@pytest.mark.parametrize("data_chunk", [(lazy_fixture("video_data_chunk"))])
+@pytest.fixture
+def images_data_chunk():
+    data_chunk = cpe.DataChunk()
+    data_chunk.add(
+        "images",
+        [(np.random.rand(10, 10, 3) * 255).astype(np.uint8) for _ in range(10)],
+    )
+    return data_chunk
+
+
+@pytest.mark.parametrize(
+    "data_chunk",
+    [
+        (lazy_fixture("video_data_chunk")),
+        (lazy_fixture("images_data_chunk")),
+    ],
+)
 def test_jsonify_data_chunk(data_chunk):
     json_data = json.dumps(data_chunk.to_json())
     new_data_chunk = cpe.DataChunk.from_json(json.loads(json_data))
