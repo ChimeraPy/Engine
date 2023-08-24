87b26c803a589ef577306c94ebf012de35adc0aa
Umesh <umesh.timalsina@vanderbilt.edu>
2023-08-23 22:40:05 -0500

    Add offset rather than substracting it

--- chimerapy/engine/clock.py
+++ chimerapy/engine/clock.py
@@ -7,19 +7,20 @@ from chimerapy.engine import config
 # Globals
 time_delta = timedelta(0)
 
+
 def sync():
     global time_delta
     ntp_client = ntplib.NTPClient()
-    for i in range(config.get('sync.attempts')):
+    for i in range(config.get("sync.attempts")):
         try:
-            response = ntp_client.request('pool.ntp.org')
+            response = ntp_client.request("pool.ntp.org")
             time_delta = timedelta(seconds=response.offset)
         except:
             time.sleep(0.5)
 
 
 def utcnow() -> datetime:
-    return datetime.utcnow() - time_delta
+    return datetime.utcnow() + time_delta
 
 
 def now() -> datetime:
