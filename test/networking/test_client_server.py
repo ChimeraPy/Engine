637c09eda141966bb2aeaa00c44533e3c2ac3d89
edavalosanaya <davalosaeduardo@gmail.com>
2023-09-08 13:32:27 -0500

    Made server and client test code async.

--- test/networking/test_client_server.py
+++ test/networking/test_client_server.py
@@ -80,8 +80,8 @@ def client_list(server):
 
 def test_server_instanciate(server):
     ...
-    
-    
+
+
 def test_server_http_req_res(server):
     r = requests.get(f"http://{server.host}:{server.port}")
     assert r.status_code == 200 and r.text == "Hello, world"
@@ -110,56 +110,63 @@ def test_server_websocket_connection_shutdown(server, client):
     server.broadcast(signal=TEST_PROTOCOL.ECHO_FLAG, data="ECHO!")
 
 
-def test_server_send_to_client(server, client):
+@pytest.mark.asyncio
+async def test_server_send_to_client(server, client):
     # Simple send
-    server.send(client_id=client.id, signal=TEST_PROTOCOL.ECHO_FLAG, data="HELLO")
+    await server.async_send(
+        client_id=client.id, signal=TEST_PROTOCOL.ECHO_FLAG, data="HELLO"
+    )
 
     # Simple send with OK
-    server.send(
+    await server.async_send(
         client_id=client.id, signal=TEST_PROTOCOL.ECHO_FLAG, data="HELLO", ok=True
     )
 
-    assert cpe.utils.waiting_for(
+    assert await cpe.utils.async_waiting_for(
         lambda: client.msg_processed_counter >= 2,
         timeout=2,
     )
 
 
-def test_client_send_to_server(server, client):
+@pytest.mark.asyncio
+async def test_client_send_to_server(server, client):
     # Simple send
-    client.send(signal=TEST_PROTOCOL.ECHO_FLAG, data="HELLO")
+    await client.async_send(signal=TEST_PROTOCOL.ECHO_FLAG, data="HELLO")
 
     # Simple send with OK
-    client.send(signal=TEST_PROTOCOL.ECHO_FLAG, data="HELLO", ok=True)
+    await client.async_send(signal=TEST_PROTOCOL.ECHO_FLAG, data="HELLO", ok=True)
 
-    assert cpe.utils.waiting_for(
+    assert await cpe.utils.async_waiting_for(
         lambda: server.msg_processed_counter >= 2,
         timeout=2,
     )
 
 
-def test_multiple_clients_send_to_server(server, client_list):
+@pytest.mark.asyncio
+async def test_multiple_clients_send_to_server(server, client_list):
 
     for client in client_list:
-        client.send(signal=TEST_PROTOCOL.ECHO_FLAG, data="ECHO!", ok=True)
+        await client.async_send(signal=TEST_PROTOCOL.ECHO_FLAG, data="ECHO!", ok=True)
 
-    assert cpe.utils.waiting_for(
+    assert await cpe.utils.async_waiting_for(
         lambda: server.msg_processed_counter >= NUMBER_OF_CLIENTS,
         timeout=5,
     )
 
 
-def test_server_broadcast_to_multiple_clients(server, client_list):
+@pytest.mark.asyncio
+async def test_server_broadcast_to_multiple_clients(server, client_list):
 
-    server.broadcast(signal=TEST_PROTOCOL.ECHO_FLAG, data="ECHO!", ok=True)
+    await server.async_broadcast(signal=TEST_PROTOCOL.ECHO_FLAG, data="ECHO!", ok=True)
 
     for client in client_list:
-        assert cpe.utils.waiting_for(
+        assert await cpe.utils.async_waiting_for(
             lambda: client.msg_processed_counter >= 2,
             timeout=5,
         )
 
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize(
     "dir",
     [
@@ -167,20 +174,20 @@ def test_server_broadcast_to_multiple_clients(server, client_list):
         (TEST_DIR / "mock" / "data" / "chimerapy_logs"),
     ],
 )
-def test_client_sending_folder_to_server(server, client, dir):
+async def test_client_sending_folder_to_server(server, client, dir):
 
     # Action
-    client.send_folder(sender_id="test_worker", dir=dir).result(timeout=10)
+    await client.async_send_folder(sender_id="test_worker", dir=dir)
 
     # Get the expected behavior
-    miss_counter = 0
-    while len(server.file_transfer_records.keys()) == 0:
+    # miss_counter = 0
+    # while len(server.file_transfer_records.keys()) == 0:
 
-        miss_counter += 1
-        time.sleep(0.1)
+    #     miss_counter += 1
+    #     time.sleep(0.1)
 
-        if miss_counter > 100:
-            assert False, "File transfer failed after 10 second"
+    #     if miss_counter > 100:
+    #         assert False, "File transfer failed after 10 second"
 
     # Also check that the file exists
     for record in server.file_transfer_records["test_worker"].values():
