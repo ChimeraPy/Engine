46ca8d629300a9e34c208dc4663efdef983c6d5c
Umesh <umesh.timalsina@vanderbilt.edu>
2023-10-12 22:06:49 -0500

    Client Server Test

---
+++ chimerapy/engine/networking/file_transfer_client.py
@@ -0,0 +1,72 @@
+import aiofiles
+import zmq
+from zmq.asyncio import Context
+
+import chimerapy.engine.config as cpe_config
+
+
+class FileTransferClient:
+    """A client for file transfer with ZeroMQ Router-Dealer pattern."""
+
+    def __init__(self, ip: str, port: int, ctx: Context, dest_name: str):
+        self.url = f"tcp://{ip}:{port}"
+        self.ctx = ctx
+        self.filename = dest_name
+
+    async def recv(self):
+        socket = self.ctx.socket(zmq.DEALER)
+        socket.connect(self.url)
+
+        credit = cpe_config.get("comms.file-transfer.max-chunks")
+        chunk_size = cpe_config.get("comms.file-transfer.chunk-size")
+
+        file = await aiofiles.open(self.filename, mode="wb")
+
+        total = 0  # Total bytes received
+        chunks = 0  # Total chunks received
+        offset = 0  # Offset of next chunk request
+
+        while True:
+            while credit:
+                await socket.send_multipart(
+                    [
+                        b"fetch",
+                        b"%i" % offset,
+                        b"%i" % chunk_size,
+                    ]
+                )
+                credit -= 1
+                offset += chunk_size
+
+            try:
+                chunk = await socket.recv()
+            except zmq.ZMQError as e:
+                if e.errno == zmq.ETERM:
+                    break
+                else:
+                    raise e
+
+            await file.write(chunk)
+            chunks += 1
+            credit += 1
+            size = len(chunk)
+            total += size
+            if size < chunk_size:
+                break
+
+            print(f"Received {total} bytes in {chunks} chunks.")
+
+        await file.close()
+
+
+if __name__ == "__main__":
+    import asyncio
+    import sys
+
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+    filename = sys.argv[3]
+
+    ctx = Context.instance()
+    client = FileTransferClient(ip, port, ctx, filename)
+    asyncio.run(client.recv())
---
+++ chimerapy/engine/networking/file_transfer_server.py
@@ -0,0 +1,70 @@
+import os
+from asyncio import Event
+from pathlib import Path
+
+import zmq
+import zmq.asyncio
+
+from chimerapy.engine import config as cpe_config
+
+
+def set_socket_hwm(socket: zmq.Socket, hwm: int):
+    socket.sndhwm = hwm
+    socket.rcvhwm = hwm
+
+
+class FileTransferServer:
+    """A server for file transfer with ZeroMQ Router-Dealer pattern."""
+
+    def __init__(self, port: int, context: zmq.asyncio.Context):
+        self._port = port
+        self._context = context
+        self._fp = None
+        self._stop_event = Event()
+
+    async def mount(self, filepath: Path):
+        assert filepath.exists(), f"File {filepath} does not exist."
+        self._fp = filepath.open("rb")
+
+    async def start(self):
+        assert self._fp is not None, "No file mounted."
+        socket = self._context.socket(zmq.ROUTER)
+        set_socket_hwm(socket, cpe_config.get("comms.file-transfer.max-chunks"))
+        socket.bind(f"tcp://*:{self._port}")
+        print(f"File transfer server started on port {self._port}.")
+        while True:
+            try:
+                msg = await socket.recv_multipart()
+            except zmq.ZMQError as e:
+                if e.errno == zmq.ETERM:
+                    break
+                else:
+                    raise e
+
+            identity, command, offset_str, chunksize_str = msg
+
+            assert command == b"fetch", f"Unknown command {command}."
+
+            offset = int(offset_str)
+            chunksize = int(chunksize_str)
+
+            self._fp.seek(offset, os.SEEK_SET)
+            data = self._fp.read(chunksize)
+
+            if not data:
+                break
+
+            await socket.send_multipart([identity, data])
+
+
+if __name__ == "__main__":
+    import asyncio
+    import sys
+
+    port = int(sys.argv[1])
+    filepath = Path(sys.argv[2])
+
+    context = zmq.asyncio.Context()
+    server = FileTransferServer(port, context)
+    asyncio.run(server.mount(filepath))
+    asyncio.run(server.start())
